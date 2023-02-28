277b0a36e2463e12a305bd1738b5cbd258367f68
MuhammadFairuza <200705045@student.ar-raniry.ac.id>
2023-02-28 14:51:05 +0700

    mulai belajar flutter

---
+++ lib/my_home_page.dart
@@ -0,0 +1,17 @@
+e
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart'
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Belajar Flutter Unit 3"),
+        centerTitle: false,
+        
+      ),
+    )
+  }
+}
